7178a750870492ea2e84e123471c558513fb2342
xXTapaXx <jgs14@hotmail.com>
2015-02-10 09:13:01 -0600

    Primer Comit
    Creacion de GitHub para el proyecto de web

---
+++ Proyecto/Models/Prueba.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto.Models
+{
+    public class Prueba
+    {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+
+
+        private bool connection_open;
+        private MySqlConnection connection;
+
+    }  
+}
